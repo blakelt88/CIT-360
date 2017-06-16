5f23e75f3b54f0a54aba0b54ad458798e37d9e0d
Blake Turley <blaketurley88@gmail.com>
2017-06-16 13:07:08 -0600

    Fix and Update

---
+++ Code Practice/Code/PUBGHibernate/src/java/InputPlayerData.java
@@ -0,0 +1,31 @@
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author Blake
+ */
+public class InputPlayerData {
+
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+
+        Player addPlayer = new Player();
+        addPlayer.setPlayerfn("Blake");
+        addPlayer.setPlayerln("Turley");
+        addPlayer.setPlayerun("GreatGoldenBaby");
+        addPlayer.setPlayeremail("blake@gmail.com");
+
+        session.save(addPlayer);
+        transaction.commit();
+
+        System.out.println("Items successfully added.");
+
+    }
+}
